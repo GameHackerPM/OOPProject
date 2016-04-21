6cd09fc107a8a58730a4ed62df9ce6b3d471a9e3
GameHackerPM <GameHackerPM@hotmail.com>
2016-04-21 20:57:33 +0200

    First Release

---
+++ Gym Managment/Gym Managment/Login.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Gym_Managment
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UNtxt = new System.Windows.Forms.TextBox();
+            this.PWtxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UNtxt
+            // 
+            this.UNtxt.Location = new System.Drawing.Point(210, 128);
+            this.UNtxt.Name = "UNtxt";
+            this.UNtxt.Size = new System.Drawing.Size(245, 22);
+            this.UNtxt.TabIndex = 0;
+            this.UNtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PWtxt
+            // 
+            this.PWtxt.Location = new System.Drawing.Point(210, 180);
+            this.PWtxt.Name = "PWtxt";
+            this.PWtxt.Size = new System.Drawing.Size(245, 22);
+            this.PWtxt.TabIndex = 1;
+            this.PWtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PWtxt.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password :";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(273, 245);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(119, 41);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 390);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PWtxt);
+            this.Controls.Add(this.UNtxt);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UNtxt;
+        private System.Windows.Forms.TextBox PWtxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
+
---
+++ Gym Managment/Gym Managment/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gym_Managment
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            bool ValidUsername = true;
+            bool ValidPassword = true;
+            //foreach (Employee emp in Program.EmpList)
+            //{
+            //    if (emp.Username == UNtxt.Text && emp.Password == PWtxt.Text)
+            //    {
+            //        ValidUsername = true;
+            //        ValidPassword = true;
+            //    }
+            //    if (emp.Username == UNtxt.Text)
+            //        ValidUsername = true;
+            //}
+            if (ValidUsername == true)//true?
+            {
+                if (ValidPassword == true)
+                {
+                    MessageBox.Show("Logged in successfully!");
+                }
+                else
+                    MessageBox.Show("Password is wrong!");
+            }
+            else
+                MessageBox.Show("Username not found!");
+        }
+    }
+}
