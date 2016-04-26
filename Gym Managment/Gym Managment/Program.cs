ff38efce1d960ce3f51331431fa98820c108d447
GameHackerPM <GameHackerPM@hotmail.com>
2016-04-26 22:16:36 +0200

    Add List for Equipment Class (By Aya Khaled)

--- Gym Managment/Gym Managment/Program.cs
+++ Gym Managment/Gym Managment/Program.cs
@@ -15,6 +15,7 @@ namespace Gym_Managment
         public static List<Transactions> TransactionsList = new List<Transactions>();
         //public static List<Suppliers> SupplierList = new List<Suppliers>();
         public static List<Instructor> InstructorsList = new List<Instructor>();
+        public static List<Equipment> EquipmentsList = new List<Equipment>();
         #endregion
 
         /// <summary>
@@ -29,6 +30,7 @@ namespace Gym_Managment
             MembersList.Add(new Members("Mariam", "Ossama", 0106985414, "3 Street Alhakam baamr allah, Shobra, Egypt.", 3, 2154, DateTime.Now.AddMonths(4)));
             MembersList.Add(new Members("Heba", "Gamal", 0106541654, "18 Street Elmo3az Mohamed, Giza, Egypt.", 2, 2654, DateTime.Now.AddMonths(1)));
             MembersList.Add(new Members("Hanaa", "Salem", 0106216578, "3 Street Korba Street, Shobra, Egypt.", 1, 1548, DateTime.Now.AddMonths(3)));
+            
             Instructor I1 = new Instructor("gamal", "soliman", "14street elrahma", 011596556, 900, DateTime.Now, 1);
             Instructor I2 = new Instructor("samy", "baher", "14street elsafa", 0115968496, 1200, DateTime.Now, 0);
             Instructor I3 = new Instructor("ibrahim", "samir", "14street elmarwa", 012759496, 4000, DateTime.Now, 2);
@@ -36,9 +38,10 @@ namespace Gym_Managment
             InstructorsList.Add(I2);
             InstructorsList.Add(I3);
            
-
-
-            
+            Equipment E1 = new Equipment("WheelSport1", 10, 1000, DateTime.Now.AddMonths(1),"sport_company");
+            Equipment E2 = new Equipment("ElectricTreadmill", 13, 1200, DateTime.Now, "fitenss_company");
+            EquipmentsList.Add(E1);
+            EquipmentsList.Add(E2);
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
