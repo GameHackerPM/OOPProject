d7e1cae62125542f64b7ccd59fdc245f3cb26305
GameHackerPM <GameHackerPM@hotmail.com>
2016-05-02 21:45:12 +0200

    Add Plan Name (By Aya)

--- Gym Managment/Gym Managment/Plan_Courses.cs
+++ Gym Managment/Gym Managment/Plan_Courses.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +9,15 @@ namespace Gym_Managment
     {
         private static int lastID = 1;
         public int ID { get; private set; }
+        public string planname;
         public int plantype;
         public double amount;
         public string details;
 
-
-        public Plan_Courses(int plantype, double amount, string details)
+        public Plan_Courses(string planname ,int plantype, double amount, string details)
         {
             ID = GetNextID();
+            this.planname = planename;
             this.plantype = plantype;
             this.amount = amount;
             this.details = details;
