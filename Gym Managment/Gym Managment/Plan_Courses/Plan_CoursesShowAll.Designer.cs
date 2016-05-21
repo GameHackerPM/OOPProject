1c095bb4d439271f084382e1b8cfc2e85023e6b4
56563 <ayatarek790.at.at@gmail.com>
2016-05-21 03:04:46 +0200

    Show All

---
+++ Gym Managment/Gym Managment/Plan_Courses/Plan_CoursesShowAll.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Gym_Managment.Plan_Courses
+{
+    partial class Plan_CoursesShowAll
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.modeCombo = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(384, 19);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ClearButton);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.modeCombo);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.searchButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(698, 363);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Plan_Courses Data Information";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(465, 20);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(101, 23);
+            this.ClearButton.TabIndex = 6;
+            this.ClearButton.Text = "Clear Filter";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 47);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(691, 306);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // modeCombo
+            // 
+            this.modeCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeCombo.FormattingEnabled = true;
+            this.modeCombo.Items.AddRange(new object[] {
+            "ID",
+            "Name",
+            "Date"});
+            this.modeCombo.Location = new System.Drawing.Point(170, 19);
+            this.modeCombo.Margin = new System.Windows.Forms.Padding(2);
+            this.modeCombo.Name = "modeCombo";
+            this.modeCombo.Size = new System.Drawing.Size(48, 21);
+            this.modeCombo.TabIndex = 2;
+            // 
+            // Plan_CoursesShowAll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 381);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Plan_CoursesShowAll";
+            this.Text = "Plan_CorsesShowAll";
+            this.Load += new System.EventHandler(this.Plan_CorsesShowAll_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox modeCombo;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+    }
+}
---
+++ Gym Managment/Gym Managment/Plan_Courses/Plan_CoursesShowAll.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gym_Managment.Plan_Courses
+{
+    public partial class Plan_CoursesShowAll : Form
+    {
+        public Plan_CoursesShowAll()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Plan_CorsesShowAll_Load(object sender, EventArgs e)
+        {
+
+            modeCombo.SelectedIndex = 0;
+
+            DataTable dt = new DataTable();
+            dt.Clear();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            
+            dt.Columns.Add("Details");
+         
+            
+            dt.Columns.Add("Amount");
+
+            foreach (Plan_Courses plan in Program.Plan_CoursesList)
+            {
+                DataRow row = dt.NewRow();
+                row["ID"] = plan.ID.ToString();
+                row["Name"] = plan.planname;
+                
+                row["details"] = plan.details;
+                
+                
+                row["Amount"] = string.Format("{0:C}", plan.amount);
+                dt.Rows.Add(row);
+            }
+            dataGridView1.DataSource = dt;
+            bool colored = false;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (colored)
+                    row.DefaultCellStyle.BackColor = Color.Silver;
+                colored = !colored;
+            }
+                
+                
+               
+               
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+            string SelectedColumn = "";
+            switch (modeCombo.SelectedIndex)
+            {
+                case 0:
+                    SelectedColumn = "ID";
+                    break;
+                case 1:
+                    SelectedColumn = "Name";
+                    break;
+                case 2:
+                    SelectedColumn = "Details";
+                    break;
+            }
+            BindingSource bs = new BindingSource();
+            bs.DataSource = dataGridView1.DataSource;
+            bs.Filter = SelectedColumn + " LIKE '%" + searchButton.Text + "%'";
+            dataGridView1.DataSource = bs;
+
+            bool colored = false;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (colored)
+                    row.DefaultCellStyle.BackColor = Color.Silver;
+                colored = !colored;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+
+            searchButton.Text = "";
+            BindingSource bs = new BindingSource();
+            bs.DataSource = dataGridView1.DataSource;
+            bs.Filter = "ID LIKE '%%'";
+            dataGridView1.DataSource = bs;
+            bool colored = false;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (colored)
+                    row.DefaultCellStyle.BackColor = Color.Silver;
+                colored = !colored;
+            }
+        }
+    }
+}
