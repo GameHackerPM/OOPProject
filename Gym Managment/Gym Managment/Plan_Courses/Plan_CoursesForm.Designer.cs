136dd518bb8ad49f77b60a23462468e909165b3d
56563 <ayatarek790.at.at@gmail.com>
2016-06-11 00:35:57 +0200

    plan courses form

---
+++ Gym Managment/Gym Managment/Plan_Courses/Plan_CoursesForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Gym_Managment.Plan_Courses
+{
+    partial class Plan_CoursesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.editbutton = new System.Windows.Forms.Button();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.detailsTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTxt = new System.Windows.Forms.TextBox();
+            this.plannameTxt = new System.Windows.Forms.TextBox();
+            this.amountTxt = new System.Windows.Forms.TextBox();
+            this.plan_coursesList = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.groupBox1.Controls.Add(this.plan_coursesList);
+            this.groupBox1.Controls.Add(this.DeleteButton);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.groupBox1.Location = new System.Drawing.Point(26, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 440);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Plan_Courses Informtion";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(51, 398);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(103, 29);
+            this.DeleteButton.TabIndex = 5;
+            this.DeleteButton.Text = "Delete ";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(38, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Plan_Courses Count  :   0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(69, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(48, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Plan_Courses List";
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Location = new System.Drawing.Point(559, 277);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(72, 32);
+            this.exitbutton.TabIndex = 43;
+            this.exitbutton.Text = "Exit";
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
+            // 
+            // editbutton
+            // 
+            this.editbutton.Location = new System.Drawing.Point(481, 277);
+            this.editbutton.Name = "editbutton";
+            this.editbutton.Size = new System.Drawing.Size(72, 32);
+            this.editbutton.TabIndex = 42;
+            this.editbutton.Text = "Edit";
+            this.editbutton.UseVisualStyleBackColor = true;
+            this.editbutton.Click += new System.EventHandler(this.editbutton_Click);
+            // 
+            // addbutton
+            // 
+            this.addbutton.Location = new System.Drawing.Point(403, 277);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(72, 32);
+            this.addbutton.TabIndex = 41;
+            this.addbutton.Text = "Add";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(325, 277);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(72, 32);
+            this.resetbutton.TabIndex = 40;
+            this.resetbutton.Text = "Reset";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Details :";
+            // 
+            // detailsTxt
+            // 
+            this.detailsTxt.Location = new System.Drawing.Point(408, 141);
+            this.detailsTxt.Multiline = true;
+            this.detailsTxt.Name = "detailsTxt";
+            this.detailsTxt.Size = new System.Drawing.Size(260, 71);
+            this.detailsTxt.TabIndex = 38;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Amount :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Plan Name :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(372, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "ID :";
+            // 
+            // idTxt
+            // 
+            this.idTxt.Location = new System.Drawing.Point(408, 12);
+            this.idTxt.Name = "idTxt";
+            this.idTxt.Size = new System.Drawing.Size(100, 20);
+            this.idTxt.TabIndex = 32;
+            // 
+            // plannameTxt
+            // 
+            this.plannameTxt.Location = new System.Drawing.Point(408, 53);
+            this.plannameTxt.Name = "plannameTxt";
+            this.plannameTxt.Size = new System.Drawing.Size(151, 20);
+            this.plannameTxt.TabIndex = 31;
+            // 
+            // amountTxt
+            // 
+            this.amountTxt.Location = new System.Drawing.Point(408, 97);
+            this.amountTxt.Name = "amountTxt";
+            this.amountTxt.Size = new System.Drawing.Size(121, 20);
+            this.amountTxt.TabIndex = 30;
+            // 
+            // plan_coursesList
+            // 
+            this.plan_coursesList.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plan_coursesList.FormattingEnabled = true;
+            this.plan_coursesList.Location = new System.Drawing.Point(6, 71);
+            this.plan_coursesList.Name = "plan_coursesList";
+            this.plan_coursesList.Size = new System.Drawing.Size(186, 316);
+            this.plan_coursesList.TabIndex = 6;
+            this.plan_coursesList.SelectedIndexChanged += new System.EventHandler(this.plan_coursesList_SelectedIndexChanged);
+            // 
+            // Plan_CoursesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 464);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.exitbutton);
+            this.Controls.Add(this.editbutton);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.detailsTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idTxt);
+            this.Controls.Add(this.plannameTxt);
+            this.Controls.Add(this.amountTxt);
+            this.Name = "Plan_CoursesForm";
+            this.Text = "Plan_CoursesForm";
+            this.Load += new System.EventHandler(this.Plan_CoursesForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox plan_coursesList;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button exitbutton;
+        private System.Windows.Forms.Button editbutton;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button resetbutton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox detailsTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idTxt;
+        private System.Windows.Forms.TextBox plannameTxt;
+        private System.Windows.Forms.TextBox amountTxt;
+    }
+}
---
+++ Gym Managment/Gym Managment/Plan_Courses/Plan_CoursesForm.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gym_Managment.Plan_Courses
+{
+    public partial class Plan_CoursesForm : Form
+    {
+        public Plan_CoursesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+            if (plannameTxt.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter plans's Plan Name!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (amountTxt.Text != string.Empty)
+            {
+                try
+                {
+                    double amount = Convert.ToDouble(amountTxt.Text); 
+                }
+                catch
+                {
+                    MessageBox.Show("Please enter amount of plan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter amount of plan!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+
+            if (detailsTxt.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter plans'details!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Plan_Courses NewPlan = new Plan_Courses(plannameTxt.Text, Convert.ToInt32(idTxt.Text), Convert.ToDouble(amountTxt.Text), detailsTxt.Text);
+
+            Program.Plan_CoursesList.Add(NewPlan);
+
+            plan_coursesList.Items.Add(NewPlan.planname);
+        }
+
+        private void Plan_CoursesForm_Load(object sender, EventArgs e)
+        {
+            foreach (Plan_Courses plan in Program.Plan_CoursesList)
+                
+            {
+                plan_coursesList.Items.Add(plan.planname);
+            
+            }
+
+            
+        }
+
+
+        private void ResetAll()
+        {
+            idTxt.Text = "";
+            plannameTxt.Text = "";
+            detailsTxt.Text = "";
+            amountTxt.Text = "$0.00";
+
+        }
+        
+        
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            ResetAll();
+        }
+
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void editbutton_Click(object sender, EventArgs e)
+        {
+
+            foreach (Plan_Courses selectedPlan in Program.Plan_CoursesList)
+            {
+                if ((string)plan_coursesList.SelectedItem == selectedPlan.planname)
+                {
+                    selectedPlan.planname = plannameTxt.Text;
+                    selectedPlan.amount = Convert.ToDouble(amountTxt.Text);
+                    selectedPlan.details = detailsTxt.Text;
+
+
+
+
+                    plan_coursesList.Items[plan_coursesList.SelectedIndex] = selectedPlan.planname;
+                    break;
+
+                }
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+
+            foreach (Plan_Courses plan in Program.Plan_CoursesList)
+            {
+                if ((string)plan_coursesList.SelectedItem == plan.planname)
+                {
+                    Program.Plan_CoursesList.Remove(plan);
+                    break;
+
+                }
+
+                plan_coursesList.Items.Remove(plan_coursesList.SelectedItem);
+
+                ResetAll();
+            } 
+            
+           
+       }
+
+        private void plan_coursesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            foreach (Plan_Courses selectedPlan in Program.Plan_CoursesList)
+            {
+                if ((string)plan_coursesList.SelectedItem == selectedPlan.planname)
+                {
+                    idTxt.Text = selectedPlan.ID.ToString();
+                    plannameTxt.Text = selectedPlan.planname;
+                    amountTxt.Text =  selectedPlan.amount.ToString();
+                    detailsTxt.Text = selectedPlan.details;
+                    
+                    break;
+                }
+            }
+        }
+    }
+ }
